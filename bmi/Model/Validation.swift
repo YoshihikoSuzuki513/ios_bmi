c6f0db240e5213e76e9c340b5865e4717aff180d
YoshihikoSuzuki <yoshihikosuzuki@main.ne.jp>
2020-02-24 16:01:24 +0900

    Validationクラス作成

---
+++ bmi/Model/Validation.swift
@@ -0,0 +1,17 @@
+//
+//  Validation.swift
+//  bmi
+//
+//  Created by 鈴木義彦 on 2020/02/24.
+//  Copyright © 2020 Yoshihiko Suzuki. All rights reserved.
+//
+
+import Foundation
+
+class Validation {
+	
+	static func isOneDecimalPlace(inputValue: Double) -> Bool {
+		return true
+	}
+	
+}
