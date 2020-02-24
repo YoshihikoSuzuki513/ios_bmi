be1ccecb08253e369f33c067fcef3a7496ad3029
YoshihikoSuzuki <yoshihikosuzuki@main.ne.jp>
2020-02-24 17:43:56 +0900

    InputViewController修正

--- bmi/Controller/InputViewController.swift
+++ bmi/Controller/InputViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class InputViewController: UIViewController {
-
+class InputViewController: UIViewController, UITextFieldDelegate {
+	
+	@IBOutlet weak var heightTextField: UITextField!
+	@IBOutlet weak var weightTextField: UITextField!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		self.view.endEditing(true)
+	}
 
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		if textField.tag == 0 {
+			weightTextField.becomeFirstResponder()
+			// TODO: 体重テキストにフォーカスする
+			return true
+		}
+		if textField.tag == 1 {
+			bmi((Any).self)
+			return true
+		}
+		return true
+	}
+	
+	@IBAction func bmi(_ sender: Any) {
+		guard let inputHeightValue = heightTextField.text else {
+			// TODO: アラート表示
+			return
+		}
+		guard let inputWeightValue = weightTextField.text else {
+			// TODO: アラート表示
+			return
+		}
+		if !Validation.isSuccsess(inputValue: inputHeightValue) {
+			// TODO: アラート表示
+			return
+		}
+		if !Validation.isSuccsess(inputValue: inputWeightValue) {
+			// TODO: アラート表示
+			return
+		}
+		let height = Height.init(value: Format.oneDecimalPlace(value: inputHeightValue))
+		let weight = Weight.init(value: Format.oneDecimalPlace(value: inputWeightValue))
+	}
+	
 }
 
